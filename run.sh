0231f2781a3e343e1cf1a54de43a503d351d8c58
coord.e <me@coord-e.com>
2018-05-04 17:21:59 +0900

    Patch yamax.urdf before training to suppress warns

--- run.sh
+++ run.sh
@@ -9,6 +9,26 @@ git fetch --all
 git pull
 [ -v DEEPL2_COMMIT_ID ] && git checkout $DEEPL2_COMMIT_ID
 
+patch << EOF
+--- YamaX_4.0.urdf	2018-05-04 16:55:18.545944675 +0900
++++ yamax.urdf	2018-05-04 16:55:09.032611604 +0900
+@@ -67,7 +67,13 @@
+     <self_collide>False</self_collide>
+     <material>Gazebo/Grey</material>
+   </gazebo>
+-  <link name="base_link"/>
++  <link name="base_link">
++    <inertial>
++      <origin xyz="0 0 0"/>
++      <mass value="0.0"/>
++      <inertia ixx="0.0" ixy="0" ixz="0" iyy="0.0" iyz="0" izz="0.0"/>
++    </inertial>
++  </link>
+   <link name="body">
+     <visual>
+       <origin rpy="0 0 0" xyz="0 0 0"/>
+EOF
+
 python3 train.py --monitor monitor --save model -se 500 --monitor-video 50000 --timesteps 10000000 --tensorboard ./tblog --discord \
   && tar Jcf $ARCHIVE_NAME monitor model tblog \
   && aws s3 cp $ARCHIVE_NAME s3://$BUCKET_NAME/
